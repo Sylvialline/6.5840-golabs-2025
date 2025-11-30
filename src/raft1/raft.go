7f4d358faf1d1465b3f5efddcafa0d7433653661
Yiming Liu <nkxjlym@gmail.com>
2025-11-30 16:50:36 +0800

    new feature: fast backup

--- src/raft1/raft.go
+++ src/raft1/raft.go
@@ -239,6 +239,28 @@ type AppendEntriesReply struct {
 	Term termT
 	// 3B
 	Success bool
+	// 3C (Fast Backup)
+	XIndex indexT
+	XTerm  termT
+}
+
+// find the largest index i <= x such that a[i].term <= k
+// a[] must be sorted in non-decreasing order
+func findLE(a []logEntry, x int, k termT) int {
+	l, r, res := 0, min(len(a)-1, x) , -1
+	if a[r].Term <= k {
+		return r
+	}
+	for l <= r {
+		mid := (l+r) >> 1
+		if a[mid].Term <= k {
+			res = mid
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return res
 }
 
 // AppendEntries RPC handler.
@@ -258,6 +280,10 @@ func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply
 	if len(rf.log) <= int(args.PrevLogIndex) ||
 	   rf.log[args.PrevLogIndex].Term != args.PrevLogTerm {
 		// do not consistent
+		x := int(args.PrevLogIndex)
+		k := args.PrevLogTerm
+		reply.XIndex = indexT(findLE(rf.log, x, k))
+		reply.XTerm = rf.log[reply.XIndex].Term
 		return
 	}
 	rf.log = rf.log[:args.PrevLogIndex+1] // trunc first: [0, prev]
@@ -537,7 +563,12 @@ func (rf *Raft) aeSender(server int) {
 			rf.commit()
 		} else {
 			// consistency check failed
-			rf.nextIndex[server] --
+			// fast backup
+			x := int(reply.XIndex)
+			k := reply.XTerm
+			idx := findLE(rf.log, x, k)
+			rf.nextIndex[server] = indexT(idx + 1)
+			// rf.nextIndex[server] --
 			ch <- true // try again
 		}
 		rf.mu.Unlock()
