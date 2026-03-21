381a6a890eb2a58d282b46da35ff4391f760a25f
Yiming Liu <nkxjlym@gmail.com>
2026-03-21 12:19:06 +0800

    3A|3B|3C tested: passed 500 times without error

--- src/raft1/test.bash
+++ src/raft1/test.bash
@@ -1,12 +1,15 @@
 set -o pipefail
-log="raft-flaky-$(date +%Y%m%d-%H%M%S).log"
+
+log="raft-$(date +%Y%m%d-%H%M%S).log"
 i=1
 
 while true; do
     echo "===== Run $i =====" | tee -a "$log"
-    if ! go test -race -run '3A|3B' 2>&1 | tee -a "$log"; then
+
+    if ! { time go test -race -run '3A|3B|3C'; } 2>&1 | tee -a "$log"; then
         echo "===== FAILED at run $i =====" | tee -a "$log"
-        $ break
+        break
     fi
-    i=$((i+1))
+
+    ((i++))
 done
